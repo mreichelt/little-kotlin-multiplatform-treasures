a92933f1e5a4685ced758b784d1afdfb4558761d
Marc Reichelt <mcreichelt@gmail.com>
2022-10-25 07:19:12 +0200

    Add iOS framework generation

--- 01_native/build.gradle.kts
+++ 01_native/build.gradle.kts
@@ -15,4 +15,13 @@ kotlin {
     mingwX64 {
         binaries { executable() }
     }
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach {
+        it.binaries.framework {
+            baseName = "shared"
+        }
+    }
 }
